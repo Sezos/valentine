7a11b2927aeb48803b0a9eb8b325c3427133c683
Badral <badral@Badrals-MacBook-Pro.local>
2024-02-07 03:08:49 +0800

    done

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC-YLYZ9JVPCz17JTW3vO_27maHERMYfA4",
+    authDomain: "valentine-4b1ef.firebaseapp.com",
+    projectId: "valentine-4b1ef",
+    storageBucket: "valentine-4b1ef.appspot.com",
+    messagingSenderId: "817392580756",
+    appId: "1:817392580756:web:120b8f476c55b821a21c81",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/App.js
+++ src/App.js
@@ -4,24 +4,45 @@ import "./App.css";
 import Gif from "./peach-and-goma-goma.gif";
 import Happy from "./happy.gif";
 import Sad from "./sad.gif";
+import Confetti from "react-confetti";
+import useWindowSize from "react-use/lib/useWindowSize";
 
 function App() {
-    const [width, setWidth] = useState(190);
     const [state, setState] = useState(0);
-    const [height, setHeight] = useState(54);
+    const [num, setNum] = useState(0);
+    const { width, height } = useWindowSize();
+    const array = [
+        "no",
+        "are you sure?",
+        "You might regret it!",
+        "Really? Think again! ",
+        "Come on, don't be shy! ",
+        "Give it a shot! Imagine all the fun we could have!",
+        "I promise it will be worth it!",
+        "Don't let this opportunity slip away! Just say yes! What do you have to lose?",
+        "You don't want to miss out on this, trust me!",
+        "I have made this especially for you!",
+        "Say yes and let the magic begin!",
+        "You are breaking my heart! I would be sad. Say yes!",
+        "Last chance! Say yes and make my day!",
+        "You are leaving me no choice! Say yes!",
+    ];
 
     const click = () => {
-        setWidth(width + 30);
-        if (height > 750) {
-            setState(-1);
-        }
-        setHeight(height + 30);
+        if (num >= array.length - 1) setState(-1);
+        else setNum(num + 1);
     };
+
     return (
         <div>
             {state === 1 ? (
                 <div className="App">
+                    <Confetti width={width} height={height} className="mt-5" />
                     <img alt="love" style={{ width: "40vh" }} src={Happy}></img>
+                    <div>
+                        Thanks for being my valentine! Will be seeing you
+                        soon!🥰
+                    </div>
                 </div>
             ) : state === 0 ? (
                 <div className="App">
@@ -45,16 +66,9 @@ function App() {
                             <div className="cover cut"></div>
                             <div className="text">Yes</div>
                         </button>
-                        <button
-                            className="Button"
-                            style={{
-                                width: `${width}px`,
-                                height: `${height}px`,
-                            }}
-                            onClick={click}
-                        >
+                        <button className="Button" onClick={click}>
                             <div className="cover cut"></div>
-                            <div className="text">No</div>
+                            <div className="text">{array[num]}</div>
                         </button>
                     </div>
                 </div>
