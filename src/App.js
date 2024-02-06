76f860f4dd45f6c3125e2286dc81c5fc12591cef
Badral <badral@Badrals-MacBook-Pro.local>
2024-02-07 02:27:02 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+// import anime from "animejs/lib/anime.es.js";
+import { useState } from "react";
+import "./App.css";
+import Gif from "./peach-and-goma-goma.gif";
+import Happy from "./happy.gif";
+import Sad from "./sad.gif";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [width, setWidth] = useState(190);
+    const [state, setState] = useState(0);
+    const [height, setHeight] = useState(54);
 
+    const click = () => {
+        setWidth(width + 30);
+        if (height > 750) {
+            setState(-1);
+        }
+        setHeight(height + 30);
+    };
+    return (
+        <div>
+            {state === 1 ? (
+                <div className="App">
+                    <img alt="love" style={{ width: "40vh" }} src={Happy}></img>
+                </div>
+            ) : state === 0 ? (
+                <div className="App">
+                    <div
+                        style={{
+                            fontWeight: 800,
+                            fontSize: "30px",
+                            color: "fuchsia",
+                        }}
+                    >
+                        Will you be My Valentine's
+                    </div>
+                    <img alt="love" style={{ width: "40vh" }} src={Gif}></img>
+                    <div className="Bottom">
+                        <button
+                            className="Button"
+                            onClick={() => {
+                                setState(1);
+                            }}
+                        >
+                            <div className="cover cut"></div>
+                            <div className="text">Yes</div>
+                        </button>
+                        <button
+                            className="Button"
+                            style={{
+                                width: `${width}px`,
+                                height: `${height}px`,
+                            }}
+                            onClick={click}
+                        >
+                            <div className="cover cut"></div>
+                            <div className="text">No</div>
+                        </button>
+                    </div>
+                </div>
+            ) : (
+                <div className="App">
+                    <img alt="love" style={{ width: "40vh" }} src={Sad}></img>
+                </div>
+            )}
+        </div>
+    );
+}
+// const style =
 export default App;
